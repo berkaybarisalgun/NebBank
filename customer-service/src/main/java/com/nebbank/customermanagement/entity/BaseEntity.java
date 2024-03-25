83d928c7b2f71f8af369381f26e97138b2ed6741
Berkay Baris Algun <bbarisalgun@gmail.com>
2024-03-25 16:43:09 +0300

    Added BasEntity and Customer entity

---
+++ customer-service/src/main/java/com/nebbank/customermanagement/entity/BaseEntity.java
@@ -0,0 +1,63 @@
+package com.nebbank.customermanagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * BaseEntity is a MappedSuperclass that provides the basic fields for auditing and soft-deleting entities.
+ * It uses the {@link AuditingEntityListener} to automatically set the createdBy, createdDate,
+ * lastModifiedBy, and lastModifiedDate fields.
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class BaseEntity {
+
+    /**
+     * The date and time the entity was created.
+     */
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    /**
+     * The user who created the entity.
+     */
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    /**
+     * The date and time the entity was last modified.
+     */
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * The user who last modified the entity.
+     */
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+
+    /**
+     * A flag indicating whether the entity is active.
+     */
+    private Boolean isActive;
+
+    /**
+     * A flag indicating whether the entity is deleted.
+     */
+    private Boolean isDeleted;
+
+}
---
+++ customer-service/src/main/java/com/nebbank/customermanagement/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.nebbank.customermanagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer extends BaseEntity{
+
+   @Id
+   @GeneratedValue
+   @Column(name = "customer_id")
+   private Long Id;
+
+   @Column(name = "first_name")
+   private String firstName;
+
+   @Column(name = "last_name")
+   private String lastName;
+
+   @Column(name = "email")
+   @Email
+   private String email;
+
+   @Column(name = "mobile_number")
+   private String mobileNumber;
+
+
+
+
+
+
+}
