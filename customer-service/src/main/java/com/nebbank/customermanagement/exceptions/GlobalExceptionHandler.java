ff61975b1876b203d265deb6f24596ff02a49bcb
Berkay Baris Algun <bbarisalgun@gmail.com>
2024-04-12 20:48:05 +0300

    Added GlobalExceptionHandler

---
+++ customer-service/src/main/java/com/nebbank/customermanagement/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.nebbank.customermanagement.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerCreationException.class)
+    public ResponseEntity<Object> handleCustomerCreationException(CustomerCreationException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "An error occurred");
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
